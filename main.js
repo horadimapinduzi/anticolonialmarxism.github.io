0b27084c0aa8b70997f137a1543560008084db5e
horadimapinduzi <malcurth@gmail.com>
2026-01-18 13:58:55 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+// Tab functionality
+function openTab(evt, tabName) {
+  // Hide all tabcontent
+  const tabcontents = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontents.length; i++) {
+    tabcontents[i].style.display = "none";
+  }
+
+  // Remove active class from all tablinks
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show current tab and mark button as active
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Initialize on page load
+window.onload = function() {
+  // Show first tab by default (Christmas List)
+  document.getElementById("Christmas List").style.display = "block";
+  document.getElementsByClassName("tablinks")[0].className += " active";
+
+  // Hide Random Gizmos tab initially
+  document.getElementById("Vegan Diet").style.display = "none";
+};
