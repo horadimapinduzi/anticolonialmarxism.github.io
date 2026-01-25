0dbcfb89b17605303e3840bb026ee2611f0db2b7
horadimapinduzi <malcurth@gmail.com>
2026-01-25 11:52:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -26,3 +26,19 @@ window.onload = function() {
   // Hide Random Gizmos tab initially
   document.getElementById("Vegan Diet").style.display = "none";
 };
+
+window.addEventListener('load', function(){
+	document.getElementById('sampleeditor').setAttribute('contenteditable', 'true');
+	document.getElementById('sampleeditor2').setAttribute('contenteditable', 'true');
+});
+
+function format(command, value) {
+	document.execCommand(command, false, value);
+}
+
+function setUrl() {
+	var url = document.getElementById('txtFormatUrl').value;
+	var sText = document.getSelection();
+	document.execCommand('insertHTML', false, '<a href="' + url + '" target="_blank">' + sText + '</a>');
+	document.getElementById('txtFormatUrl').value = '';
+}
